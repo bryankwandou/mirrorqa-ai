7dec5b002f4c9cb8d1738006a707c5b01a5d965c
Vincentius Bryan Kwandou <wall.breaker.king.commander@gmail.com>
2026-07-28 21:43:57 +0800

    Trace Chromium binaries in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, poweredByHeader: false, experimental: { serverComponentsExternalPackages: ["playwright-core", "@sparticuz/chromium"] } };
+const nextConfig = { reactStrictMode: true, poweredByHeader: false, experimental: { serverComponentsExternalPackages: ["playwright-core", "@sparticuz/chromium"], outputFileTracingIncludes: { "/api/browser/run": ["./node_modules/@sparticuz/chromium/bin/**"] } } };
 
 export default nextConfig;
